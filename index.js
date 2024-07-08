22b34e4ff331aad00003860d595745028a9bf20a
Paulo de Tarso F. Machado <paulodetarsofm@yahoo.com.br>
2024-07-08 11:48:01 +0100

    Add `cors`

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -79,7 +81,8 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
